4072550a421ad657eb9674796200b16694ed2ea0
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-03 20:35:11 +0900

    Update task.js

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -1,13 +1,7 @@
 $(function(){
   
-  if ($('.day').has($('i'))) {
-    let html = $('i').parent().parent().find('a').has(('.new_task_link'));
+  if ($('.day').hasClass($('.has-events'))) {
+    let html = $('.has-events').find('a').has(('.new_task_link'));
     html.remove();
   }
-
-  if ($('.day').has($('.task_body'))) {
-    let html = $('.task_body').parent().parent().find('a').has($('.new_task_link'));
-    html.remove();
-  }
-
 })
