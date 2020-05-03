41c70b5e56bb4be55676fadc791ee0430ad49b01
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-03 17:05:19 +0900

    create dialy link

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -1,7 +1,12 @@
 $(function(){
   
   if ($('.day').has($('i'))) {
-    let html = $('i').parent().find('a').has('.new_task_link');
+    let html = $('i').parent().parent().find('a').has(('.new_task_link'));
+    html.remove();
+  }
+
+  if ($('.day').has($('.task_body'))) {
+    let html = $('.task_body').parent().parent().find('a').has($('.new_task_link'));
     html.remove();
   }
 
