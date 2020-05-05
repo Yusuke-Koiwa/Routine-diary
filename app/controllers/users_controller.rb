013c079dc5682969086640eb07dd6689d5bab173
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-05 21:45:44 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   before_action :move_to_root_path, unless: :user_signed_in?
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])
     @tasks = @user.tasks
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   private
@@ -11,4 +25,13 @@ class UsersController < ApplicationController
     flash.now[:alert] = "ログインが必要です"
     render "welcome/home"
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :image)
+  end
+
 end
