5bb2012f370aa25073bd7a1dda9e201c0402cc8c
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-07 14:55:57 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,23 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   def done_days(tasks)
-    day_count = tasks.where(score: "done").count
+    sorted_tasks = tasks.where("date <= ?", Date.today)
+    day_count = sorted_tasks.where(score: "done").count
   end
 
   def continuous_days(tasks)
     sorted_tasks = tasks.where("date <= ?", Date.today).order(date: "DESC")
-    length = sorted_tasks.length
-    sorted_tasks.each_with_index do |task, index|
-      if task.score != "done"
-        return index
-      end
-      if index == length - 1
-        return index + 1
+    if sorted_tasks.first.date < Date.today - 1
+      return 0
+    else
+      length = sorted_tasks.length
+      sorted_tasks.each_with_index do |task, index|
+        if task.score != "done"
+          return index
+        end
+        if index == length - 1
+          return index + 1
+        end
       end
     end
   end
