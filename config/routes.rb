d9d39cafff77605a10307243ce1b96114dac55b3
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-11 08:44:41 +0900

    define routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#home"
   resources :users
-  resources :tasks
   resources :routines, except: [:index, :show]
+  resources :tasks, except: [:edit]
+  post "praises/:task_id/create" => "praises#create"
+  post "praises/:task_id/destroy" => "praises#destroy"
 end
