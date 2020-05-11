bf172ec8324fa2af33a9c53121dc244936e1c8a3
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-11 12:32:06 +0900

    Update praises_controller.rb

--- app/controllers/praises_controller.rb
+++ app/controllers/praises_controller.rb
@@ -1,7 +1,12 @@
 class PraisesController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    @praised_tasks = @user.praised_tasks
+  end
   
   def create
-    @praise = Praise.create(task_id: params[:task_id], user_id: current_user.id)
+    @praise = Praise.create(praise_params)
     redirect_back(fallback_location: root_path)
   end
 
@@ -11,4 +16,8 @@ class PraisesController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  private
+  def praise_params
+    params.permit(:task_id).merge(user_id: current_user.id)
+  end
 end
