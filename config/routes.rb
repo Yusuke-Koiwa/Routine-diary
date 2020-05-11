7022239c40b186da633ee792ed147fc48b94e506
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-11 12:33:31 +0900

    define routing praises#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :routines, except: [:index, :show]
   resources :tasks, except: [:edit]
+  get "praises/:user_id/index" => "praises#index"
   post "praises/:task_id/create" => "praises#create"
   post "praises/:task_id/destroy" => "praises#destroy"
 end
