b24562ed93e72d343fe4b17738c33a15d29841fd
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-11 21:16:19 +0900

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,31 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
+
+  def create
+    following = current_user.follow(@user)
+    if following.save
+      flash[:notice] = "#{@user}さんをフォローしました"
+      redirect_to @user
+    else
+      flash[:alert] = "フォローに失敗しました"
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      flash[:notice] = "#{@user}さんのフォローを解除しました"
+      redirect_to @user
+    else
+      flash[:alert] = "フォローの解除に失敗しました"
+      redirect_to @user
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
