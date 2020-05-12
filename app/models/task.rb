4a6d0b41cb77f8cc8a63fc9cebad9b82fbf6355c
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-12 19:30:55 +0900

    define association

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :comments
   has_many :praises, dependent: :destroy
   has_many :praised_users, through: :praises, source: :user
   validates :user_id, presence: true
