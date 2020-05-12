1e334dc85a2c783691ead32dbdd80d2fe9961140
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-12 19:30:20 +0900

    generate Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_074311) do
+ActiveRecord::Schema.define(version: 2020_05_12_102233) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "praises", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "task_id"
@@ -63,6 +73,8 @@ ActiveRecord::Schema.define(version: 2020_05_11_074311) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users"
   add_foreign_key "praises", "tasks"
   add_foreign_key "praises", "users"
   add_foreign_key "relationships", "users"
