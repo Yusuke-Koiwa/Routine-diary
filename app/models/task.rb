2116091bf2187591d1fcef453d32a0b07e1653ad
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-13 18:32:59 +0900

    delete task with comments

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :praises, dependent: :destroy
   has_many :praised_users, through: :praises, source: :user
   validates :user_id, presence: true
