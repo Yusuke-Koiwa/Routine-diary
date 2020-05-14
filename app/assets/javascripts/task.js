c07714d2926054a1a850867b60ed5c686c187fdf
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-14 16:37:02 +0900

    Update task.js

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -4,4 +4,19 @@ $(function(){
     let html = $('.has-events').find('a').has(('.new_task_link'));
     html.remove();
   }
+
+  $("#praise-tab").on('click', function(){
+    $("#comment-tab").removeClass("selected");
+    $("#praise-tab").addClass("selected");
+    $("#comments-window").hide();
+    $("#praised_user-window").show();
+  })
+
+  $("#comment-tab").on('click', function(){
+    $("#praise-tab").removeClass("selected");
+    $("#comment-tab").addClass("selected");
+    $("#praised_user-window").hide();
+    $("#comments-window").show();
+  })
+
 });
