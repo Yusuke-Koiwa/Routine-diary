93be0c832722154eeb3a15c2134c731701e725f5
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-14 19:12:07 +0900

    define method of comment_user_image_url

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :task
   validates :content, presence: true
+
+  def user_image_url(user)
+    if self.user.image?
+      return user.image.url
+    else
+      return "/assets/default.jpg"
+    end
+  end
 end
