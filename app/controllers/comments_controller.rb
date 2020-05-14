bc1795b42ca335f3b2977a3056d949432279935c
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-14 19:12:14 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
 def create
   @comment = Comment.new(comment_params)
+  @user_image_url = @comment.user_image_url(@comment.user)
   if @comment.save
     respond_to do |format|
       format.json
