6622846d73c4423849222a874b097d1777783116
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-15 12:42:47 +0900

    Update praises_controller.rb

--- app/controllers/praises_controller.rb
+++ app/controllers/praises_controller.rb
@@ -3,20 +3,10 @@ class PraisesController < ApplicationController
   
   def create
     @praise = current_user.praise(@task)
-    if @praise.save
-      redirect_back(fallback_location: tasks_path)
-    else
-      redirect_back(fallback_location: tasks_path)
-    end
   end
 
   def destroy
     @praise = current_user.unpraise(@task)
-    if @praise.destroy
-      redirect_back(fallback_location: tasks_path)
-    else
-      redirect_back(fallback_location: tasks_path)
-    end
   end
 
   private
