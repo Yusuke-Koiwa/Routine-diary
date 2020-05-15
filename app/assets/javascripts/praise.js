3ece42a58a1e3aaaa9ec1ebed47ec668f7bf8573
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-15 12:44:02 +0900

    Update praise.js

--- app/assets/javascripts/praise.js
+++ app/assets/javascripts/praise.js
@@ -1,3 +1,9 @@
 $(function(){
 
+  $(".praise-btn").on('click', function(){
+    if ($(this).children("a").hasClass("good-job--on")) {
+      let id = $(".current_user_id").val();
+      $("#praised_user-box").children(`#user-${id}`).remove();
+    } 
+  });
 });
