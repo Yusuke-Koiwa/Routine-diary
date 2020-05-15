ba145cb2aee0c5a167db81245097a15f4fc0e702
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-15 14:56:12 +0900

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,24 +3,10 @@ class RelationshipsController < ApplicationController
 
   def create
     following = current_user.follow(@user)
-    if following.save
-      flash[:notice] = "#{@user.name}さんをフォローしました"
-      redirect_back(fallback_location: user_path(@user))
-    else
-      flash[:alert] = "フォローに失敗しました"
-      redirect_back(fallback_location: user_path(@user))
-    end
   end
 
   def destroy
     following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:notice] = "#{@user.name}さんのフォローを解除しました"
-      redirect_back(fallback_location: user_path(@user))
-    else
-      flash[:alert] = "フォローの解除に失敗しました"
-      redirect_back(fallback_location: user_path(@user))
-    end
   end
 
   private
