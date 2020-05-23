61acb28e9dcd7c792da2fbee76d5ec8cd4980c9b
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-24 08:44:34 +0900

    delete unnecessary  files

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,39 +1,15 @@
 $(function(){
 
-  $("#signup-btn").on('click', function(){
-    $("#modal-wrapper").fadeIn();
-    $("#new-registration").fadeIn();
-  })
-
-  $("#login-btn").on('click', function(){
-    $("#modal-wrapper").fadeIn();
-    $("#new-session").fadeIn();
-  })
-
   $(".close-btn").on('click', function(){
     $("#modal-wrapper").fadeOut();
-    $("#new-registration").fadeOut();
-    $("#new-session").fadeOut();
     $("#new-task").fadeOut();
     $("#edit-task").fadeOut();
     $("#new-routine").fadeOut();
     $(".edit-routine").fadeOut();
   })
 
-  $("#login-link").on('click', function(){
-    $("#new-registration").hide("fast");
-    $("#new-session").show("fast");
-  })
-
-  $("#signup-link").on('click', function(){
-    $("#new-session").hide("fast");
-    $("#new-registration").show("fast");
-  })
-
   $("#modal-wrapper").on('click', function(){
     $("#modal-wrapper").fadeOut();
-    $("#new-registration").fadeOut();
-    $("#new-session").fadeOut();
     $("#new-task").fadeOut();
     $("#edit-task").fadeOut();
     $("#new-routine").fadeOut();
