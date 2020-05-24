67de4b6d7dc3f31d529070d5b46eada1b07f698d
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-24 12:31:16 +0900

    add association routine and category

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,5 +1,6 @@
 class Routine < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
   belongs_to :user
   validates :content, :user_id, presence: true
 
