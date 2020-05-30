6218a9e396678dfb12a142f58dec0e83a29dcbfa
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-30 18:43:22 +0900

    add validation of maximum number of routines

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,7 +1,16 @@
 class Routine < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to_active_hash :category
   belongs_to :user
-  validates :content, :user_id, presence: true
+
+  validates :content, :category_id, presence: true
+  validate :routines_number, on: :create
+
+  def routines_number
+    if user && user.routines.count >= 3
+      errors.add(:routine, "登録できる習慣は3つまでです")
+    end
+  end
 
 end
