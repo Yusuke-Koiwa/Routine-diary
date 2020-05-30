969b867a3065a26e0f398b3879488514054f2750
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-30 17:26:25 +0900

    add validation to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,10 @@ class Task < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :praises, dependent: :destroy
   has_many :praised_users, through: :praises, source: :user
-  validates :user_id, presence: true
+
+  validates :date, :start_time, presence: true
+  validates :score, presence: true, unless: :body?
+  validates :body, presence: true, unless: :score?
 
   def create_routine_log(user)
     user.routines.each do |r|
