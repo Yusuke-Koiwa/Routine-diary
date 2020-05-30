d94c31b47b9647aa75ecdf5e2a7cef0092a2a874
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-30 14:34:06 +0900

    update migration file of user

--- db/migrate/20200501032559_devise_create_users.rb
+++ db/migrate/20200501032559_devise_create_users.rb
@@ -4,9 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, defalut: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false
+      t.string :image
+      t.boolean :admin,             default: false, null: false
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
