d517abb1e0c892c5b5185c1922577284cbe48903
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-31 09:02:44 +0900

    Create routine_log_spec.rb

---
+++ spec/models/routine_log_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe RoutineLog do
+
+  describe '#create' do
+
+    it "content, date, category_id, task_idの全てが存在すれば登録できる" do
+      routine_log = build(:routine_log)
+      expect(routine_log).to be_valid
+    end
+
+    it "contentが存在しない場合は登録できない" do
+      routine_log = build(:routine_log, content: nil)
+      routine_log.valid?
+      expect(routine_log.errors[:content]).to include("を入力してください")
+    end
+
+    it "dateが存在しない場合は登録できない" do
+      routine_log = build(:routine_log, date: nil)
+      routine_log.valid?
+      expect(routine_log.errors[:date]).to include("を入力してください")
+    end
+
+    it "category_idが存在しない場合は登録できない" do
+      routine_log = build(:routine_log, category_id: nil)
+      routine_log.valid?
+      expect(routine_log.errors[:category_id]).to include("を入力してください")
+    end
+
+    it "task_idが存在しない場合は登録できない" do
+      routine_log = build(:routine_log, task_id: nil)
+      routine_log.valid?
+      expect(routine_log.errors[:task]).to include("を入力してください")
+    end
+
+  end
+
+end
