81cc8a1d7b44b7c85ed0b40747c605d5d261ad34
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-31 14:46:29 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
   factory :user do
     name                  {"hoge"}
     image                 {"hoge.png"}
-    email                 {"hoge@hoge.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
+    sequence(:email)      {Faker::Internet.email}
   end
 
 end
