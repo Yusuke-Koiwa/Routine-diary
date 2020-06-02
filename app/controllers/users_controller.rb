8c9ee00420f662f4b149df991a05b4c1240147ba
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-03 08:34:51 +0900

    prohibit to delete admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,7 @@ class UsersController < ApplicationController
 
   def admin_user?
     redirect_to root_path unless current_user.admin?
+    redirect_to root_path if User.find(params[:id]).admin?
   end
 
 end
