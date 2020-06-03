0af7d604e55274a99d831726fbd7a7c4e8799544
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-03 09:22:31 +0900

    create controller test coad of routines_create

---
+++ spec/controllers/routines/create_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+describe RoutinesController do
+  let(:user) { create(:user) }
+  let(:params) { {routine: {content: "hoge", category_id: 1}} }
+
+  describe '#create' do
+
+    context 'ログインしている場合' do
+      before do
+        login user
+        routine1 = create(:routine, user: user)
+        routine2 = create(:routine, user: user)
+      end
+
+      context 'routineの登録数が2つ以下の場合' do
+        subject { post :create, params: params }
+        it 'routineを保存する' do
+          expect{ subject }.to change(Routine, :count).by(1)
+        end
+        it 'マイページにリダイレクトする' do
+          subject
+          expect(response).to redirect_to(user_path(user))
+        end
+      end
+
+      context 'routineが既に3つ存在する場合' do
+        before do
+          routine3 = create(:routine, user: user)
+        end
+        subject { post :create, params: params }
+
+        it 'routineを保存しない' do
+          expect{ subject }.not_to change(Routine, :count)
+        end
+        it 'マイページにリダイレクトする' do
+          subject
+          expect(response).to redirect_to(user_path(user))
+        end
+      end
+
+    end
+    
+    context 'ログインしていない場合' do
+      subject { post :create, params: params }
+      it 'routineを保存しない' do
+        expect{ subject }.not_to change(Routine, :count)
+      end
+      it 'ログインページにリダイレクトする' do
+        subject
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+end
