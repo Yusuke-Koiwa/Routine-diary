d2cb5629c54d1ac631d472cfb50b798779effe11
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-06 17:23:57 +0900

    add association between category and routine

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  has_many :rooutines
   has_many :routine_logs
   has_many :tasks, through: :routine_logs
 end
--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,7 +1,7 @@
 class Routine < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
-  belongs_to_active_hash :category
+  belongs_to :category
   belongs_to :user
 
   validates :content, :category_id, presence: true
