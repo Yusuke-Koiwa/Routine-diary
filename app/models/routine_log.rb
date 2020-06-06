3c82b6018b0d31e863fda0908c6cd5a8a2acb61e
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-06 14:30:43 +0900

    fix association

--- app/models/routine_log.rb
+++ app/models/routine_log.rb
@@ -1,6 +1,6 @@
 class RoutineLog < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to :category
   belongs_to :task
 
   validates :content, :date, :category_id, presence: true
