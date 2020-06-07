365eda2b9ff3e181d0935d7590b471c71741edb6
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-07 09:08:18 +0900

    add associations involved in Notification

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,7 @@ class Task < ApplicationRecord
   has_many :praises, dependent: :destroy
   has_many :praised_users, through: :praises, source: :user
   has_many :categories, through: :routine_logs
+  has_many :notifications, dependent: :destroy
 
   validates :date, :start_time, presence: true
   validates :score, presence: true, unless: :body?
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :praises, dependent: :destroy
   has_many :praised_tasks, through: :praises, source: :task
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
   has_many :relationships, dependent: :destroy
   has_many :follow_users, through: :relationships, source: :follow
