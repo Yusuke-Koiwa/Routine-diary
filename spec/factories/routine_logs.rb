98b6c0639da478fee5fb6c2caabff236d8e15747
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-18 15:50:36 +0900

    refactoring: NumericLiteralPrefix

--- spec/factories/routine_logs.rb
+++ spec/factories/routine_logs.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :routine_log do
     task
     content           { "hogehogehogehogehoge" }
-    date              { Date.new(2020, 05, 30) }
+    date              { Date.new(2020, 0o5, 30) }
     category
   end
 end
