fd46a85f291d8f78966e2c82bb73c6d62d6ee881
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-18 14:06:21 +0900

    refactor image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,21 +18,21 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    #   # For Rails 3.1+ asset pipeline compatibility:
+    #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     # "default.jpg"
     "/default.jpg"
   end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+    # Process files as they are uploaded:
+    # process scale: [200, 300]
+    #
+    # def scale(width, height)
+    #   # do something
+    # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
+    # Create different versions of your uploaded files:
+    # version :thumb do
     process resize_to_fit: [100, 100]
   # end
 
