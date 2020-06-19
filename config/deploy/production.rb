99a7ae8bb6a6731b16ef6eb216de09a1aae4f80e
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-19 12:18:19 +0900

    refactoring: RercentLiteralDelimiters

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '54.238.29.248', user: 'ec2-user', roles: %w{app db web}
+server '54.238.29.248', user: 'ec2-user', roles: %w[app db web]
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
